ae26f333f4e5f81ae1ac7f2466977305b6ff48e4
Laura Fiestas <100427798+zambiel@users.noreply.github.com>
2024-11-12 12:05:13 +0100

    Add files via upload

---
+++ CV-LauraFiestas/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ProfileCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
